300a75f122dece4ca3c239d4edbb3ca3f733d6f4
cesar reyes <97656884+Khesartt@users.noreply.github.com>
2022-12-18 19:16:16 -0500

    V 2.3
    agregar el primer controlador, configuraciones varias y yap :)

---
+++ Plantilla_S_EF/Controllers/CitiesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Plantilla_S_EF.Context;
+using Plantilla_S_EF.Models;
+
+namespace Plantilla_S_EF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly PrincipalContext _context;
+
+        public CitiesController(PrincipalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> Getcities()
+        {
+            return await _context.cities.ToListAsync();
+        }
+
+        // GET: api/Cities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> GetCity(int id)
+        {
+            var city = await _context.cities.FindAsync(id);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return city;
+        }
+
+        // PUT: api/Cities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCity(int id, City city)
+        {
+            if (id != city.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(city).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<City>> PostCity(City city)
+        {
+            _context.cities.Add(city);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCity", new { id = city.id }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await _context.cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            _context.cities.Remove(city);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CityExists(int id)
+        {
+            return _context.cities.Any(e => e.id == id);
+        }
+    }
+}
